e66c1d5a1a9c8c886355d352530d47dc4b44c5e9
RGNGIT <68756004+RGNGIT@users.noreply.github.com>
2021-04-02 19:28:11 +0500

    Add files via upload

---
+++ _TRPO_Lab1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace _TRPO_Lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Введите число:");
+                SimplifyCout(Convert.ToInt32(Console.ReadLine()));
+            }
+            catch(FormatException)
+            {
+                Console.WriteLine("Введен неверный формат числа или не число!");
+            }
+            Console.ReadKey();
+        }
+
+        static void SimplifyCout(int Value)
+        {
+            for(int i = 0; i < Value; i++)
+            {
+                if(isSimple(i))
+                {
+                    Console.Write($"{i} ");
+                }
+            }
+        }
+
+        static bool isSimple(int Num)
+        {
+            if (Num == 4)
+            {
+                return false;
+            }
+            for (int i = 2; i < Num / 2; i++)
+            {
+                if(Num % i == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+    }
+}
