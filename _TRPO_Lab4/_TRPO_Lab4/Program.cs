aa81261724a1a398326b624d3c4b1bfc23e750c8
RGN <vnifedow@bk.ru>
2021-05-02 22:32:06 +0500

    Прога

--- _TRPO_Lab4/_TRPO_Lab4/Program.cs
+++ _TRPO_Lab4/_TRPO_Lab4/Program.cs
@@ -1,15 +1,24 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace _TRPO_Lab4
 {
     class Program
     {
+
+        static double Func(double x) => Math.Sqrt(Math.Pow(x, 2) - 0.64) / x;
+
         static void Main(string[] args)
         {
+            double a = double.Parse(Console.ReadLine());
+            double b = double.Parse(Console.ReadLine());
+            double Sum = 0;
+            for(double i = a; i < b; i += 0.1)
+            {
+                Sum += Func(i);
+                Console.WriteLine($"Функция: {Func(i)} Сумма: {Sum}");
+            }
+            Console.ReadKey();
         }
+
     }
 }
