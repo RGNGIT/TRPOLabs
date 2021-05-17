4082354484f8703b3083ac20eec74e633b2d4e2e
БЕЗДОТНая Хоопс ✶ ДоскаИгры <vnifedow@bk.ru>
2021-05-17 15:44:03 +0500

    +

--- _TRPO_Lab4/_TRPO_Lab4/Program.cs
+++ _TRPO_Lab4/_TRPO_Lab4/Program.cs
@@ -7,15 +7,24 @@ namespace _TRPO_Lab4
 
         static double Func(double x) => Math.Sqrt(Math.Pow(x, 2) - 0.64) / x;
 
+        static bool Check(double a, double b)
+        {
+
+            return true;
+        }
+
         static void Main(string[] args)
         {
             double a = double.Parse(Console.ReadLine());
             double b = double.Parse(Console.ReadLine());
-            double Sum = 0;
-            for(double i = a; i < b; i += 0.1)
+            if (Check(a, b))
             {
-                Sum += Func(i);
-                Console.WriteLine($"Функция: {Func(i)} Сумма: {Sum}");
+                double Sum = 0;
+                for (double i = a; i < b; i += 0.1)
+                {
+                    Sum += Func(i);
+                    Console.WriteLine($"Функция: {Func(i)} Сумма: {Sum}");
+                }
             }
             Console.ReadKey();
         }
