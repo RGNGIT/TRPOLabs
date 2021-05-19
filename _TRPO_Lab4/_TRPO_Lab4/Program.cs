fbfded143e36fd3e258ac94a443144d25671849c
RGN <vnifedow@bk.ru>
2021-05-19 11:43:14 +0500

    +

--- _TRPO_Lab4/_TRPO_Lab4/Program.cs
+++ _TRPO_Lab4/_TRPO_Lab4/Program.cs
@@ -6,25 +6,33 @@ namespace _TRPO_Lab4
     {
 
         static double Func(double x) => Math.Sqrt(Math.Pow(x, 2) - 0.64) / x;
-
-        static bool Check(double a, double b)
+        // ОДЗ: Все, кроме от -0.8 до 0.8
+        static int NullReturner()
         {
-
-            return true;
+            Console.WriteLine("Выход за ОДЗ!");
+            return 0;
         }
 
-        static void Main(string[] args)
+        static void Main()
         {
             double a = double.Parse(Console.ReadLine());
             double b = double.Parse(Console.ReadLine());
-            if (Check(a, b))
+            if (a < b)
             {
                 double Sum = 0;
                 for (double i = a; i < b; i += 0.1)
                 {
-                    Sum += Func(i);
-                    Console.WriteLine($"Функция: {Func(i)} Сумма: {Sum}");
+                    Sum += !Double.IsNaN(Func(i)) ? Func(i) : NullReturner();
+                    if (!Double.IsNaN(Func(i)))
+                    {
+                        Console.WriteLine($"Функция: {Func(i)} Сумма: {Sum}");
+                    }
                 }
+            }
+            else
+            {
+                Console.WriteLine("Низ цикла больше верха");
+                Main();
             }
             Console.ReadKey();
         }
